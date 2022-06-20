64ac1f87f35dcdad1909a77da83de9eab72c0892
Alex <akomanov@seatgeek.com>
2022-06-20 16:14:30 +0300

    Lesson 2 code

---
+++ SachlavClassWork/src/lesson_2_variables/Variables.java
@@ -0,0 +1,26 @@
+package lesson_2_variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte myBytePlus = 30;
+        short myShort = 23432;
+        int myInt = 2147483647;
+        long myLong = 21474836478L;
+
+        float myFloat = 2.345f;
+        double myDouble = 4.567;
+
+        boolean myBoolean = false;
+        boolean myBoolean2 = true;
+
+        char myChar = 'd';
+        char MyChar = '\u03A9';
+        char MyChar2 = '9';
+        System.out.println(MyChar);
+
+        String myString = "Alex";
+        String myLastName = new String("Komanov"); 
+    }
+}
