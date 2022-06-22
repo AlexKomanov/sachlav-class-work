0f13fbc29c4c604a92068ac3840379c0c1c63a2b
Alex <akomanov@seatgeek.com>
2022-06-22 10:17:07 +0300

    Task 1 if-else

--- SachlavClassWork/src/lesson_3_operators/tasks/UserSystem.java
+++ SachlavClassWork/src/lesson_3_operators/tasks/UserSystem.java
@@ -1,29 +1,29 @@
-package lesson_3_operators;
+package lesson_3_operators.tasks;
 
 import java.util.Scanner;
 
 public class UserSystem {
     public static void main(String[] args) {
 
-
         String login = "Admin";
-        String password = "P@ssw0rd";
-        // На 10 строке создаём объект класса Scanner для обработки ввода с клавиатуры
+        String password = "P@ssword";
+
+        // Создаём объект класса Scanner для обработки ввода с клавиатуры
         Scanner in = new Scanner(System.in);
 
         System.out.print("Введите login: ");
 
-        // На 19 строке создаем строкову локальную переменную с именем usersLogin
+        // Cоздаём строкову локальную переменную с именем usersLogin
         // и принимаем в нее ввод от пользователя
 
         String usersLogin = in.nextLine();
 
         if (login.equals(usersLogin)) {
             System.out.print("Введите password: ");
-            String usersPassword = in.next();
+            String usersPassword = in.nextLine();
 
             if (password.equals(usersPassword)) {
-                System.out.println("Здравствуйте " + usersLogin + ", Вы вошли в систему.");
+                System.out.println("Здравствуйте " + login + ", Вы вошли в систему.");
             } else {
                 System.out.println("Вы ввели неверный пароль!");
             }
