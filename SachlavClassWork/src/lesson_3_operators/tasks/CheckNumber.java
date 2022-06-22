4e236293edcee6d55d7cc3e90e63d67c7f34abf0
Alex <akomanov@seatgeek.com>
2022-06-22 10:39:04 +0300

    Task CheckNumber

---
+++ SachlavClassWork/src/lesson_3_operators/tasks/CheckNumber.java
@@ -0,0 +1,25 @@
+package lesson_3_operators.tasks;
+
+import java.util.Scanner;
+
+public class CheckNumber {
+    public static void main(String[] args) {
+
+        // Создаём объект класса Scanner для обработки ввода с клавиатуры
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+
+        // Cоздаём строкову локальную переменную с именем usersLogin
+        // и принимаем в нее ввод от пользователя
+
+        int number = in.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.print("Число " + number  + " чётное.");
+
+        } else {
+            System.out.print("Число " + number  + " нечётное.");
+        }
+    }
+}
